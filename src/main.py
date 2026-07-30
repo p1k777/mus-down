3326aaf62fba8ccac9ecaf99fc1fcda66077509a
Igor <kondratyevigor200730@gmail.com>
2026-07-30 11:07:09 +0500

    artists setting added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,23 @@
 from yt_dlp import YoutubeDL
+from mutagen.id3 import ID3, TIT2, TPE1
+
+def set_metainfo(path: str, title: str, arists: list[str]):
+    tags = ID3(path)
+
+    tags.delall("TIT2")
+    tags.delall("TPE1")
+
+    tags.add(TIT2(
+        encoding=3,
+        text=[title],
+    ))
+
+    tags.add(TPE1(
+        encoding=3,
+        text=', '.join(arists),
+    ))
+
+    tags.save(path, v2_version=4)
 
 YDL_OPTS = {
     "format": "bestaudio/best",
@@ -23,10 +42,14 @@ def work():
 
     with YoutubeDL(YDL_OPTS) as ydl:
         info = ydl.extract_info(url, download=True)
-        print("[AVAILABLE KEYS]", info.keys())
-        print(
-            f'"{info["title"]}" {info["creators"]} -> {info["requested_downloads"][0]["filepath"]}'
-        )
+
+        title = info["title"]
+        artists = info["creators"]
+        path = info["requested_downloads"][0]["filepath"]
+
+        set_metainfo(path, title, artists)
+
+        print(f'"{title}" {artists} -> {path}')
 
 
 
